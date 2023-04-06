9d686bd0cb53a1014f68fdfea34f6d4a3e44224a
koro.it <c1106237@planet.kanazawa-it.ac.jp>
2023-04-06 22:02:23 +0900

    [Add] Assets

---
+++ Assets/Script/PianoClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class PianoClick: UdonSharpBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
